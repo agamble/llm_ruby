a2fd9844d39b2f104438ba69ea33d2da8570d198
Alex Gamble <agamble.uk@gmail.com>
2024-09-14 00:17:33 +0100

    Add gemini chat support

---
+++ lib/llm/clients/gemini.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require "httparty"
+require "event_stream_parser"
+
+class LLM::Clients::Gemini
+  include HTTParty
+  base_uri "https://generativelanguage.googleapis.com"
+
+  def initialize(llm:)
+    @llm = llm
+  end
+
+  def chat(messages, options = {})
+    req = Request.new(messages, options)
+
+    resp = post_url(
+      "/v1beta/models/#{llm.canonical_name}:generateContent",
+      body: req.params.to_json
+    )
+
+    Response.new(resp).to_normalized_response
+  end
+
+  private
+
+  attr_reader :llm
+
+  def post_url(url, **kwargs)
+    self.class.post(url, **kwargs.merge(headers: default_headers))
+  end
+
+  def default_headers
+    {
+      "x-goog-api-key" => (ENV["GEMINI_API_KEY"]).to_s,
+      "Content-Type" => "application/json"
+    }
+  end
+end
