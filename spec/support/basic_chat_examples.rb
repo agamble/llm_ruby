0a4ca25061de9f200fc7ee150b6e7f54a1f1b0ef
Alex Gamble <agamble.uk@gmail.com>
2025-02-23 14:38:50 +0000

    Reorganise tests

--- spec/support/basic_chat_examples.rb
+++ spec/support/basic_chat_examples.rb
@@ -7,26 +7,26 @@ RSpec.shared_examples "a model that supports basic chat" do |llm_canonical_name:
   let(:client) { described_class.new(llm: llm) }
   subject(:response) { client.chat([{role: :user, content: "hello world"}]) }
 
-  it "returns a response for model #{llm_canonical_name}" do |example|
-    with_vcr(example) do
+  it "returns a response" do |example|
+    with_vcr_for_model(llm, example) do
       expect(response).to be_a(LLM::Response)
     end
   end
 
-  it "returns a response with content for model #{llm_canonical_name}" do |example|
-    with_vcr(example) do
+  it "returns a response with content" do |example|
+    with_vcr_for_model(llm, example) do
       expect(response.content).to be_a(String)
     end
   end
 
-  it "returns a response with a stop reason for model #{llm_canonical_name}" do |example|
-    with_vcr(example) do
+  it "returns a response with a stop reason" do |example|
+    with_vcr_for_model(llm, example) do
       expect(response.stop_reason).to be(LLM::StopReason::STOP)
     end
   end
 
-  it "returns a response with a raw response for model #{llm_canonical_name}" do |example|
-    with_vcr(example) do
+  it "returns a response with a raw response" do |example|
+    with_vcr_for_model(llm, example) do
       expect(response.raw_response).to_not be_nil
     end
   end
