84cdca6d15ead1c656f35e415b969be7988bb337
Alex Gamble <agamble.uk@gmail.com>
2025-02-23 17:32:38 +0000

    Add basic support for openai structured outputs

--- lib/llm/response.rb
+++ lib/llm/response.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 
-LLM::Response = Struct.new(:content, :raw_response, :stop_reason, keyword_init: true)
+require "ostruct"
+
+LLM::Response = Struct.new(:content, :raw_response, :stop_reason, :structured_output, keyword_init: true) do
+  def structured_output_object
+    return nil unless structured_output
+
+    OpenStruct.new(structured_output)
+  end
+end
